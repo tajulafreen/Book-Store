ac4cf1ec03cb2139eaff3ae1204b6e45ca49c43a
afreen shaik <affushaik9493@gmail.com>
2023-07-24 22:59:21 +0530

    add redux

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
